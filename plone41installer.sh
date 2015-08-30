420d76bbb269ba464ab7648e9ce43dfc1577fcb1
David Bain <pigeonflight@gmail.com>
2015-08-30 12:47:46 -0500

    Update plone41installer.sh

--- plone41installer.sh
+++ plone41installer.sh
@@ -1,3 +1,28 @@
+python_url=https://www.python.org/ftp/python/2.6.9/Python-2.6.9.tgz
+python_file=Python-2.6.9.tgz
+python_name=Python-2.6.9
+
+#wget $python_url
+#tar xvfz $python_file
+##cd $python_name
+#./configure
+##make
+#sudo make install
+#cd ..
+
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install libxslt1-dev
+sudo apt-get install libbz2-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install python-setuptools
+sudo apt-get install python-dev
+sudo apt-get install libjpeg62-dev
+sudo apt-get install libreadline-gplv2-dev
+sudo apt-get install python-imaging
+sudo apt-get install wv
+sudo apt-get install poppler-utils
 # spinner borrowed from http://fitnr.com/showing-a-bash-spinner.html
 
 spinner()
@@ -16,13 +41,14 @@ spinner()
 }
 
 echo "getting the installer"
-wget http://launchpad.net/plone/4.1/4.1/+download/Plone-4.1-UnifiedInstaller-20110907.tgz
+wget https://launchpad.net/plone/4.1/4.1.6/+download/Plone-4.1.6-UnifiedInstaller.tgz
 echo "unpacking the installer"
-tar xfz Plone-4.1-UnifiedInstaller-20110907.tgz
-cd Plone-4.1-UnifiedInstaller
+tar xfz Plone-4.1.6-UnifiedInstaller.tgz
+cd Plone-4.1.6-UnifiedInstaller
 
 echo "installing (first pass)"
-./install.sh standalone --target=$HOME/workspace &
+sudo apt-get install libssl-dev
+./install.sh standalone --target=$HOME/workspace --libz=yes  &
 spinner $!
 
 
