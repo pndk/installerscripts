21c8df50992f4c322d2733d8f0aed4b2cbdb529d
David Bain <pigeonflight@gmail.com>
2015-09-09 00:26:32 -0500

    Create plone5latestinstaller.sh

---
+++ plone5latestinstaller.sh
@@ -0,0 +1,42 @@
+# spinner borrowed from http://fitnr.com/showing-a-bash-spinner.html
+
+spinner()
+{
+    local pid=$1
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+echo "getting the installer"
+wget https://launchpad.net/plone/5.0/5.0rc1/+download/Plone-5.0rc1-UnifiedInstaller.tgz
+echo "unpacking the installer"
+tar xfz Plone-5.0rc1-UnifiedInstaller.tgz
+cd Plone-5.0rc1-UnifiedInstaller
+
+echo "installing (first pass)"
+./install.sh standalone --target=$HOME/workspace &
+spinner $!
+
+
+echo "***********************************"
+echo "**                               **"
+echo "** Plone install complete        **"
+echo "**                               **"
+echo "** the commands will not work in **"
+echo "**  this console. Close this     **"
+echo "**  terminal                     **"
+echo "**  in a newly opened terminal   **"
+echo "**  then and run:                **"
+echo "**                               **"
+echo "**     cd zinstance              **"
+echo "**     bin/instance fg           **"
+echo "**                               **"
+echo "***********************************"
