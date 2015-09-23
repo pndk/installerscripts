1abb4e9686e96a2f6bb60c4f5adaf4fe1b3771a2
David Bain <pigeonflight@gmail.com>
2015-09-23 14:52:02 -0500

    Update plone5latestinstaller.sh

--- plone5latestinstaller.sh
+++ plone5latestinstaller.sh
@@ -16,10 +16,10 @@ spinner()
 }
 
 echo "getting the installer"
-wget https://launchpad.net/plone/5.0/5.0rc2/+download/Plone-5.0rc2-UnifiedInstaller.tgz
+wget https://launchpad.net/plone/5.0/5.0rc3/+download/Plone-5.0rc3-UnifiedInstaller.tgz
 echo "unpacking the installer"
-tar xfz Plone-5.0rc2-UnifiedInstaller.tgz
-cd Plone-5.0rc2-UnifiedInstaller
+tar xfz Plone-5.0rc3-UnifiedInstaller.tgz
+cd Plone-5.0rc3-UnifiedInstaller
 
 echo "installing (first pass)"
 ./install.sh standalone --target=$HOME/workspace &
